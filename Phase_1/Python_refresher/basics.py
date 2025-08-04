abea558a529fc8b0683ccbe27dfe6cf7ae47fac0
SAI PRASAD <sp12961369@gmail.com>
2025-08-04 20:56:06 +0530

    Python Basics

--- Phase_1/Python_refresher/basics.py
+++ Phase_1/Python_refresher/basics.py
@@ -5,3 +5,27 @@ x = 10
 y = 20
 z = x + y
 print(z)
+
+### Conditionals
+x = int(input("Enter a number: "))
+y = 15
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
+
+### Loops
+for i in range(10):
+    print(i)
+
+while x < 20:
+    x += 1
+    print(x)
+
+### Functions
+def add(x, y):
+    return x + y
+
+print(add(10, 20))
